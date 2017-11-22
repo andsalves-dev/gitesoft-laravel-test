143654d5c6ef72eb2d13f31c9094769c980665e7
Anderson <andsalves@alu.ufc.br>
2017-11-21 22:40:55 -0300

    Films Restful API routes and Controller / GET films ready

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +13,8 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::resource('films', 'FilmsRestController');
